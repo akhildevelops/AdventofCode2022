2cb4e2764673efe5c8d2ffea051595ffec3ddca9
enforcer007 <akhilgodvsdemon@gmail.com>
2022-12-01 22:31:29 +0530

    day 1 solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use adventofcode_2022::*;
 fn main() {
-    println!("Hello, world!");
+    let day1 = Day1::from_input_file(&format!("./src/day1/{}", INPUT_FILE_NAME));
+    println!("part-1:{},part-2:{}", day1.part1(), day1.part2());
 }
