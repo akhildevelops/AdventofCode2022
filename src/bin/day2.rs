06b32a6ec0e9b3185e3140e5e1ae6c104b3c8557
enforcer007 <akhilgodvsdemon@gmail.com>
2022-12-04 11:50:54 +0530

    restructure code

---
+++ src/bin/day2.rs
@@ -0,0 +1,6 @@
+use adventofcode_2022::*;
+fn main() {
+    // Day2 Solution
+    let mut day2 = Day2::from_input_file(&format!("./src/day2/{}", INPUT_FILE_NAME));
+    println!("part-1:{},part-2:{}", day2.part1(), day2.part2());
+}
---
+++ src/bin/day3.rs
@@ -0,0 +1,6 @@
+use adventofcode_2022::*;
+fn main() {
+    // Day3 Solution
+    let mut day3 = Day3::from_input_file(&format!("./src/day3/{}", INPUT_FILE_NAME));
+    println!("part-1:{},part-2:{}", day3.part1(), day3.part2());
+}
