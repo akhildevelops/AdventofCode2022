1ef2bfb4afb44c558ff94dc52d5b97ce34184534
enforcer007 <akhilgodvsdemon@gmail.com>
2022-12-07 07:36:53 +0530

    day6

--- README.md
+++ README.md
@@ -15,11 +15,12 @@ The solutions are written in [Rust](https://www.rust-lang.org/) a high performan
 
 To get each day's solution, clone this repo: https://github.com/akhildevelops/AdventofCode2022 and run respective command as shown below
 
-| Day | Story | Solution | Part1 | Part2 | command to run |
-| --- | --- | --- | --- | --- | --- |
-| Dec-1 | [Calorie Counting](https://adventofcode.com/2022/day/1) | [Day1](./src/day1/mod.rs) 🚢 | ⭐ | ⭐  | `cargo solve day1`
-| Dec-2 | [Rock Paper Scissors](https://adventofcode.com/2022/day/2) | [Day2](./src/day2/mod.rs) 🚢 | ⭐ | ⭐ | `cargo solve day2`
-| Dec-3 | [Rucksack Reorganization](https://adventofcode.com/2022/day/3) | [Day3](./src/day3/mod.rs) 🚢 | ⭐ | ⭐ | `cargo solve day3`
-| Dec-4 | [Camp Cleanup](https://adventofcode.com/2022/day/4) | [Day4](./src/day4/mod.rs) 🚢 | ⭐ | ⭐ | `cargo solve day4`
-| Dec-5 | [Supply Stacks](https://adventofcode.com/2022/day/5) | [Day5](./src/day5/mod.rs) 🚢 | ⭐ | ⭐ | `cargo solve day5`
+| Day | Story | Solution | Part1 | Part2 | command to run | challenge level |
+| --- | --- | --- | --- | --- | --- | --- |
+| Dec-1 | [Calorie Counting](https://adventofcode.com/2022/day/1) | [Day1](./src/day1/mod.rs) 🚢 | ⭐ | ⭐  | `cargo solve day1` | 🧗
+| Dec-2 | [Rock Paper Scissors](https://adventofcode.com/2022/day/2) | [Day2](./src/day2/mod.rs) 🚢 | ⭐ | ⭐ | `cargo solve day2`|🧗 🧗
+| Dec-3 | [Rucksack Reorganization](https://adventofcode.com/2022/day/3) | [Day3](./src/day3/mod.rs) 🚢 | ⭐ | ⭐ | `cargo solve day3`| 🧗🧗
+| Dec-4 | [Camp Cleanup](https://adventofcode.com/2022/day/4) | [Day4](./src/day4/mod.rs) 🚢 | ⭐ | ⭐ | `cargo solve day4`|🧗🧗
+| Dec-5 | [Supply Stacks](https://adventofcode.com/2022/day/5) | [Day5](./src/day5/mod.rs) 🚢 | ⭐ | ⭐ | `cargo solve day5`| 🧗🧗🧗
+| Dec-5 | [Tuning Trouble](https://adventofcode.com/2022/day/6) | [Day6](./src/day6/mod.rs) 🚢 | ⭐ | ⭐ | `cargo solve day6`|🧗
 
--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -2,116 +2,51 @@ use crate::{Day, FromFile, FromStr};
 use std::fmt::Display;
 #[derive(Debug, Clone)]
 pub struct Day6 {
-    stack: Vec<Vec<char>>,
-    moves: Vec<(u32, u32, u32)>,
-}
-
-// From https://stackoverflow.com/questions/64498617/how-to-transpose-a-vector-of-vectors-in-rust
-fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>>
-where
-    T: Clone,
-{
-    assert!(!v.is_empty());
-    (0..v[0].len())
-        .map(|i| v.iter().map(|inner| inner[i].clone()).collect::<Vec<T>>())
-        .collect()
-}
-
-fn stack_parser(x: &str) -> Vec<Vec<char>> {
-    let mut lines = x.lines();
-    lines.next_back();
-    let lines = lines
-        .map(|x| {
-            let n = x.len();
-            (0..n)
-                .step_by(4)
-                .map(|y| {
-                    if &x[y..y + 3] != "   " {
-                        Some(*&x.chars().nth(y + 1).unwrap())
-                    } else {
-                        None
-                    }
-                })
-                .collect()
-        })
-        .collect();
-    let lines = transpose(lines);
-    lines
-        .iter()
-        .map(|x| {
-            x.iter()
-                .filter(|y| y.map_or(false, |_| true))
-                .map(|y| y.unwrap())
-                .collect()
-        })
-        .collect()
-}
-
-fn move_parser(x: &str) -> Vec<(u32, u32, u32)> {
-    x.lines()
-        .map(|x| {
-            let mut moves = x
-                .split(" ")
-                .map(|x| x.parse::<u32>().ok())
-                .filter(|x| x.map_or(false, |_| true));
-            (
-                moves.next().unwrap().unwrap(),
-                moves.next().unwrap().unwrap(),
-                moves.next().unwrap().unwrap(),
-            )
-        })
-        .collect()
+    stream: Vec<char>,
 }
 
 impl FromStr for Day6 {
     fn from_input(contents: String) -> Self {
-        let mut s_m = contents.split("\n\n");
-        let stack = s_m.next().unwrap();
-        let moves = s_m.next().unwrap();
-
         Self {
-            stack: stack_parser(stack),
-            moves: move_parser(moves),
+            stream: contents.chars().collect(),
         }
     }
 }
 
-impl FromFile for Day6 {}
-impl Day<String, String> for Day6 {
-    fn part1(&mut self) -> String {
-        let mut s = self.clone();
-        for &move_crate in s.moves.iter() {
-            let from_c = s.stack.get_mut(move_crate.1 as usize - 1).unwrap();
-            let movable_crates = from_c[..move_crate.0 as usize].to_vec();
-            from_c.drain(..move_crate.0 as usize);
-            let to_c = s.stack.get_mut(move_crate.2 as usize - 1).unwrap();
-
-            for each_crate in movable_crates.iter() {
-                to_c.insert(0, *each_crate)
+impl Day6 {
+    fn get_start_marker(&self, length: usize) -> u32 {
+        let n = self.stream.len();
+        let mut n_chars: usize = 0;
+        let mut first_flag: bool = false;
+        for index in 0..=n - length {
+            let mut chars_4 = self.stream[index..index + length].to_vec();
+            for s_i in 0..length {
+                let anchor_c = chars_4.remove(s_i);
+                if !chars_4.contains(&anchor_c) {
+                    if s_i == length - 1 {
+                        first_flag = true;
+                        n_chars = index + length;
+                    }
+                } else {
+                    break;
+                }
+                chars_4.insert(s_i, anchor_c);
+            }
+            if first_flag {
+                break;
             }
         }
-        s.stack
-            .iter()
-            .map(|x| x[0].to_string())
-            .reduce(|x, y| x + &y)
-            .unwrap()
+        n_chars as u32
     }
-    fn part2(&mut self) -> String {
-        for &move_crate in self.moves.iter() {
-            let from_c = self.stack.get_mut(move_crate.1 as usize - 1).unwrap();
-            let movable_crates = from_c[..move_crate.0 as usize].to_vec();
-            from_c.drain(..move_crate.0 as usize);
-            let to_c = self.stack.get_mut(move_crate.2 as usize - 1).unwrap();
+}
 
-            for each_crate in movable_crates.iter().rev() {
-                to_c.insert(0, *each_crate)
-            }
-        }
-        self.stack
-            .iter()
-            .map(|x| x[0].to_string())
-            .reduce(|x, y| x + &y)
-            .unwrap()
+impl FromFile for Day6 {}
+impl Day<u32, u32> for Day6 {
+    fn part1(&mut self) -> u32 {
+        self.get_start_marker(4)
+    }
+    fn part2(&mut self) -> u32 {
+        self.get_start_marker(14)
     }
 }
 
@@ -119,16 +54,7 @@ impl Day<String, String> for Day6 {
 mod tests {
     use super::*;
 
-    const input: &str = "    [D]    
-[N] [C]    
-[Z] [M] [P]
- 1   2   3 
-
-move 1 from 2 to 1
-move 3 from 1 to 3
-move 2 from 2 to 1
-move 1 from 1 to 2
-";
+    const input: &str = "nppdvjthqldpwncqszvftbrmjlhg";
 
     #[test]
     fn test_moves_stack() {
@@ -136,11 +62,11 @@ move 1 from 1 to 2
     }
     #[test]
     fn test_part1() {
-        assert_eq!(Day6::from_input(input.to_string()).part1(), "CMZ")
+        assert_eq!(Day6::from_input(input.to_string()).part1(), 6)
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(Day6::from_input(input.to_string()).part2(), "MCD")
+        assert_eq!(Day6::from_input(input.to_string()).part2(), 23)
     }
 }
